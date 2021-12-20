c45fd99a5744e1090d7d16c7bac5578e956cd83a
Viraj Patidar <vrj381@gmail.com>
2021-12-20 21:08:31 +0530

    volunteer model

--- map/models.py
+++ map/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Volunteer(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255, unique=True)
+    age = models.IntegerField()
+    phone = models.IntegerField()
+    city = models.CharField(max_length=20)
+    pincode = models.IntegerField()
+
+    
+    def __str__(self):
+        return f"{self.name}"
