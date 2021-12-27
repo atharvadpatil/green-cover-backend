04dffe0bcb4a46c62d7cecfb11d3a0ff2fdfcc40
atharvadpatil <atharva.dp@somaiya.edu>
2021-12-27 18:35:52 +0530

    prod6

--- greenCover/settings.py
+++ greenCover/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['green-cover.netlify.app', '127.0.0.1']
+ALLOWED_HOSTS = ['green-cover.netlify.app', 'green-cover-backend.herokuapp.com']
 
 
 # Application definition
